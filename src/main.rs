70f85718bd3cd2c4828f557a65093d89410901d3
John Children <john.a.children@gmail.com>
2020-06-27 12:32:59 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+
+mod commands;
+
+use std::{
+    collections::HashSet,
+    env,
+    sync::Arc,
+};
+use serenity::{
+    client::bridge::gateway::ShardManager,
+    framework::{
+        StandardFramework,
+        standard::macros::group,
+    },
+    model::{event::ResumedEvent, gateway::Ready},
+    prelude::*,
+};
+use kankyo;
+use tracing::{error, info, Level};
+
+use commands::{
+    meta::*
+};
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(ping)]
+struct General;
+
+fn main() {
+    // This will load the environment variables located at `./.env`, relative to
+    // the CWD. See `./.env.example` for an example on how to structure this.
+    kankyo::init().expect("Failed to load .env file");
+
+    // a builder for `FmtSubscriber`.
+    let subscriber = tracing_subscriber::fmt()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::INFO)
+        // completes the builder and sets the constructed `Subscriber` as the default.
+        .init();
+
+    
+
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    {
+        let mut data = client.data.write();
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
+
+    let owners = match client.cache_and_http.http.get_current_application_info() {
+        Ok(info) => {
+            let mut set = HashSet::new();
+            set.insert(info.owner.id);
+
+            set
+        },
+        Err(why) => panic!("Couldn't get application info: {:?}", why),
+    };
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c
+            .owners(owners)
+            .prefix("~"))
+        .group(&GENERAL_GROUP));
+
+    if let Err(why) = client.start() {
+        error!("Client error: {:?}", why);
+    }
+}
